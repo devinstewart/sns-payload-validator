3e96f86deae05c47c0c264c51046ee1ed9e345ee
Devin Stewart <devin@lucysoft.net>
2023-01-09 21:28:44 -0600

    reorganize index.d.ts files to match AWS SDK style (#42)

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,15 +1,2 @@
-import { SnsPayload } from '../interfaces';
-declare class Validator {
-
-    /**
-    * Validates an SNS payload and returns a Promise or passes it to the callback if provided.
-    *
-    * @param payload - The payload as a valid JSON string or Object sent in the HTTP/S body or Lambda event.
-    * @param callback - Optional callback, if not passed a Promise is returned.
-    * @returns If no callback is passed, a Promise is returned.
-    */
-    validate(payload: SnsPayload|String, callback?: (err: Error, payload: SnsPayload) => void): Promise<SnsPayload>;
-}
-
-declare const validator: Validator;
-export = validator;
+import SnsPayloadValidator from '../interfaces';
+export = SnsPayloadValidator;
